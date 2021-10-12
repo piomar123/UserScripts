b8635c487bc652e24134352f5b2286ea8e1d2323
piomar123 <piomar123@gmail.com>
2021-10-12 16:13:59 +0200

    Use jQuery noConflict to michau napraw tagi

--- Chrono-reader.user.js
+++ Chrono-reader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chrono-reader
 // @namespace    https://github.com/piomar123/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Przeglądanie powiadomień na wykop.pl w kolejności chronologicznej
 // @author       piomar123
 // @match        http://www.wykop.pl/*
@@ -13,7 +13,9 @@
 // ==/UserScript==
 // Badge bullet style by krejdd
 
-$('div#nav ul.clearfix:last > li.notification.m-tag').last().before($('<li/>', {
+var jq = $.noConflict();
+
+jq('div#nav ul.clearfix:last > li.notification.m-tag').last().before(jq('<li/>', {
     id: 'chronoTag',
     html: '<a id="chronoTagButton" class="dropdown-show hashtag" href="#"><b id="chronoTagBullet" style="background:#0080FF; top:22px; right:16px; min-width:10px; width:10px; min-height:10px; height:10px; border-radius:5px; overflow:hidden;">&nbsp;</b>#</a><div class="notificationsContainer"></div>',
     class: "notification m-tag piomar-chronotag",
@@ -21,7 +23,7 @@ $('div#nav ul.clearfix:last > li.notification.m-tag').last().before($('<li/>', {
     alt: "Najstarszy nieprzeczytany tag"
 }));
 
-$("#chronoTagButton").click(function(event){
+jq("#chronoTagButton").click(function(event){
     event.preventDefault();
     var tagiURL = "/powiadomienia/tagi/";
     var notificationSelector = '#content ul.menu-list li.type-light-warning a[href*="pl/wpis/"]';
@@ -34,15 +36,15 @@ $("#chronoTagButton").click(function(event){
         currentTotal: 0,
         oldestUrl: null
     };
-    $.ajax(tagiURL)
+    jq.ajax(tagiURL)
         .done(firstPageDownloaded(data))
         .fail(handleFail);
 
     function firstPageDownloaded(data){
         return function(html){
             html = parse(html);
-            data.unreadCount = Number($('#hashtagsNotificationsCount', html).html());
-            var notifications = $(notificationSelector, html);
+            data.unreadCount = Number(jq('#hashtagsNotificationsCount', html).html());
+            var notifications = jq(notificationSelector, html);
             data.currentTotal = notifications.length;
             data.oldestUrl = notifications.last().attr("href");
             if(data.currentTotal < data.unreadCount){
@@ -62,7 +64,7 @@ $("#chronoTagButton").click(function(event){
     }
 
     function downloadPage(data, page){
-        $.ajax(tagiURL + "strona/" + page + "/")
+        jq.ajax(tagiURL + "strona/" + page + "/")
             .done(nextPageDownloaded(data, page))
             .fail(handleFail);
     }
@@ -71,7 +73,7 @@ $("#chronoTagButton").click(function(event){
         return function(html){
             html = parse(html);
             //data.unreadCount = Number($('#hashtagsNotificationsCount', html).html());
-            var notifications = $(notificationSelector, html);
+            var notifications = jq(notificationSelector, html);
             // possible race condition
             data.currentTotal += notifications.length;
             data.oldestUrl = notifications.last().attr("href") || data.oldestUrl;
@@ -89,11 +91,11 @@ $("#chronoTagButton").click(function(event){
 
     function parse(html){
         html = html.replace(/<img[^>]*>/g, "");
-        return $.parseHTML(html);
+        return jq.parseHTML(html);
     }
 
     function checkPageNotEmpty(html){
-        return $(emptyPageAlertSelector, html).length === 0;
+        return jq(emptyPageAlertSelector, html).length === 0;
     }
 
     function handleFail(jqXHR, textStatus, err){
